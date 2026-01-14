40b9b12efffd654b7fbbf60cb4b1eb46d37af292
Bollavaram Harika <96771877+Bollavara@users.noreply.github.com>
2026-01-14 10:11:04 +0530

    Delete labs/Q1 - 4.py

--- labs/Q1 - 4.py
+++
@@ -1,7 +0,0 @@
-from functools import reduce
-n=[1,2,3,4,5,6,7,8,9,10]
-even=filter(lambda x:x%2==0,n)
-a=filter(lambda x:x%2==0,n) #Even numbers
-z=map(lambda x:x*x,a) #square numbers
-result=reduce(lambda x,y:x+y,z) #sum of squared numbers
-print(result)
