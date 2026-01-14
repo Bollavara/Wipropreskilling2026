f423e16385731e3bc39a99ed9508831162506e6a
Bollavaram Harika <96771877+Bollavara@users.noreply.github.com>
2026-01-14 17:31:09 +0530

    Add files via upload

---
+++ labs/Day 3/Q 1 - 2.py
@@ -0,0 +1,18 @@
+import time
+
+def execution_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+
+        print("Execution time:", end_time - start_time, "seconds")
+        print(f"Function name: {func.__name__}")
+        return result
+    return wrapper
+@execution_time
+def sample_function():
+    for i in range(1000000):
+        pass
+
+sample_function()
